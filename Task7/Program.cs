0375295d96f34b9bc165dfb91649bb005f438c8d
miha2158 <miha.vanya@gmail.com>
2017-09-05 11:21:04 +0500

    Начало положено

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using static System.Console;
 
 namespace Task7
 {
     class Program
     {
+        static string CheckInput(string input)
+        {
+            if (input.Length <= 1)
+                return "Слишком короткая функция";
+
+            bool p = false;
+            foreach (char c in input)
+                if (c != '0' || c != '1' || c != '*')
+                {
+                    p = true;
+                    break;
+                }
+            if (p)
+                return "Ошибка. Возможные символы: '0', '1', '*'.";
+
+            double value = Math.Log(input.Length, 2);
+            if ((int)value != value)
+                return "Булева функция должна иметь длину равную степени двойки";
+            return string.Empty;
+        }
+
         static void Main(string[] args)
         {
+            string input;
+            WriteLine("Введите булеву функцию. Возможные символы: '0', '1', '*'.");
+
+            while (true)
+            {
+                input = ReadLine();
+                string p = CheckInput(input);
+                if(p == string.Empty)
+                    break;
+                WriteLine(p);
+            }
+
+
+
+
+
+
         }
     }
 }
